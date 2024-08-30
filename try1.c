a4f1b337873b9c9d64816539ca66af57704b6e20
Fishir123 <faiqthestrongest@gmail.com>
2024-08-31 05:04:05 +0700

    make if decission

---
+++ try1.c
@@ -0,0 +1,16 @@
+#include <Stdio.h>
+
+void main(){
+    printf("==Program Pembayaran==\n");
+    int total_belanja = 0;
+
+    printf("Inputkan total belanja: ");
+    scanf(%i, &total_belanja);
+
+    //menggunakan blok percabangan if
+    if(total_belanja > 100000){
+        printf("Selamat anda mendapatkan hadiah!\n");
+    }
+
+    Printf("Terimakasih sudah berbelanja di toko kami \n\n");
+}
